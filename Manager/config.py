29833d82f278001d2bee44701ce911911ae8b5c5
kalya <stella.probot@gmail.com>
2021-05-21 23:25:35 +0530

    Update config.py

--- Manager/config.py
+++ Manager/config.py
@@ -27,10 +27,10 @@ class Config(object):
 
     # OPTIONAL
     #ID Seperation format [1,2,3,4]
-    SUDO_USERS = (1603318426)  # List of id's -  (not usernames) for users which have sudo access to the bot.
-    DEV_USERS = (1603318426)  # List of id's - (not usernames) for developers who will have the same perms as the owner
-    SUPPORT_USERS = (1603318426)  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = (1603318426)  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    SUDO_USERS = [1603318426]  # List of id's -  (not usernames) for users which have sudo access to the bot.
+    DEV_USERS = [1603318426]  # List of id's - (not usernames) for developers who will have the same perms as the owner
+    SUPPORT_USERS = [1603318426]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = [1603318426]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
