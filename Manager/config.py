8033fdbc434fb4fcbf06f48e11613b7c98a45ef8
Akeno <82025071+Stella-80@users.noreply.github.com>
2021-06-29 12:28:49 +0530

    Update config.py

--- Manager/config.py
+++ Manager/config.py
@@ -11,26 +11,26 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "1700341854:AAGW_H-5wmLJZhmTbru0a0jRs2QfdpbHeTU"
+    API_KEY = "1880462737:AAFfviTXYI5KY9zVN8cznVpdimNE0SK9Zqw"
     OWNER_ID = "1461968113"  # If you dont know, run the bot and do /id in your private chat with it
     OWNER_USERNAME = "itzz_axel"  #Just write it without the @ ex "Sawada"
-    SUPPORT_CHAT = "akenosupportchat" # Your telegram support chat username, must have the "@" Example: @MyBotSupportGroupChat
+    SUPPORT_CHAT = "Vexana_support" # Your telegram support chat username, must have the "@" Example: @MyBotSupportGroupChat
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'postgresql://stella:stella123@database-1.cuzno6euao1o.us-east-2.rds.amazonaws.com:5431/dbname'  # needed for any database modules
-    MESSAGE_DUMP = -1001395712266  # needed to make sure 'save from' messages persist
-    GBAN_LOGS = -1001395712266 #Channel ID here with the hyphen like -123456789
+    SQLALCHEMY_DATABASE_URI = 'postgres://phfzgnhf:yZqY10tmLDviJW7cZCiCcQfO6d48cav3@batyr.db.elephantsql.com/phfzgnhf'  # needed for any database modules
+    MESSAGE_DUMP = -1001452059200  # needed to make sure 'save from' messages persist
+    GBAN_LOGS = -1001452059200 #Channel ID here with the hyphen like -123456789
     LOAD = []
-    NO_LOAD = ['translation', 'rss', 'cleaner', 'feds']   
+    NO_LOAD = ['translation', '-1001452059200rss', 'cleaner', 'feds']   
     WEBHOOK = False
     URL = None
 
     # OPTIONAL
     #ID Seperation format [1,2,3,4]
-    SUDO_USERS = [1603318426]  # List of id's -  (not usernames) for users which have sudo access to the bot.
-    DEV_USERS = [1603318426]  # List of id's - (not usernames) for developers who will have the same perms as the owner
-    SUPPORT_USERS = [1603318426]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = [1603318426]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    SUDO_USERS = [1461968113]  # List of id's -  (not usernames) for users which have sudo access to the bot.
+    DEV_USERS = [1461968113]  # List of id's - (not usernames) for developers who will have the same perms as the owner
+    SUPPORT_USERS = [1461968113]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = [1461968113]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
